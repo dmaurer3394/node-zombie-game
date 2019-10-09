86919e24e5e1ca045837a6b8b764fa4d6edfbbcc
Dalton Maurer <dmaurer3394@gmail.com>
2019-10-09 02:28:28 -0400

    added new random conditions

--- zombiegame.js
+++ zombiegame.js
@@ -1,6 +1,6 @@
 var inquirer = require("inquirer");
 var userHealth = 100;
-var zombieHealth = 30;
+var zombieHealth = 50;
 
 console.log("\nYou're caught in a zombie apocolypse!\nOn your daily trip to the city to find food, you encountered a zombie horde.\nYou were able to distract most of the herd, but one of them saw you!\nHe's running straight at you!");
 guessNumber();
@@ -19,9 +19,9 @@ function guessNumber() {
     console.log("\nThe zombie's health: " + zombieHealth + "\n");
 
     var randomToGuess = Math.floor(Math.random() * 5) + 1;
-    var randomHitPoints = Math.floor(Math.random() * 5) + 1;
-    var userCritNumber = Math.floor(Math.random() * 15) + 1;
-    var critChance = Math.floor(Math.random() * 15) + 1;
+    var randomHitPoints = Math.floor(Math.random() * 10) + 1;
+    var zombieCritNumber = Math.floor(Math.random() * 10) + 1;
+    var critChance = Math.floor(Math.random() * 10) + 1;
 
     inquirer
         .prompt([
@@ -33,32 +33,40 @@ function guessNumber() {
 
         ])
         .then(function(result) {
+            var userInput = parseFloat(result.number);
+            var numberPlusOne = userInput + 1;
+            var numberMinusOne = userInput - 1;
 
             console.log("===============================")
             // console.log("random to guess: " + randomToGuess); 
             // console.log("random hit points " + randomHitPoints); 
-            // console.log("user crit number: " + userCritNumber);
-            // console.log("crit chance " + critChance)
+            // console.log("user crit number: " + zombieCritNumber);
+            // console.log("crit chance " + critChance);
+            // console.log(parseInt(userInput));
+            // console.log(parseInt(numberPlusOne));
+            // console.log(parseInt(numberMinusOne));
             // console.log("===============================")
 
-            if (userCritNumber === critChance) {
-                randomHitPoints = randomHitPoints * 2
-            }
-
-            if (userCritNumber === critChance) {
-                // console.log("critical hit points: " + randomHitPoints);
-                console.log("----- CRITICAL HIT -----");
-            };
-
-            if (result.number == randomToGuess) {
-                console.log("Great guess!\n");
-                console.log("You got him for " + randomHitPoints + " hit points!");
-                zombieHealth -= randomHitPoints;
-            } else {
-                console.log("Wrong guess...\n");
-                console.log("He got you for " + randomHitPoints + " hit points!");
+            if (zombieCritNumber === critChance) {
+                randomHitPoints = randomHitPoints * 2;
+                console.log("----- CRITICAL MISS -----");
+                console.log("yOu ReAlLy WhIfFeD iT...");
+                console.log("He hit you for " + randomHitPoints + " hit points")
                 userHealth -= randomHitPoints;
-            }
+            } else if (userInput === randomToGuess) {
+                randomHitPoints = randomHitPoints * 2;
+                console.log("----- CRITICAL HIT -----");
+                console.log("You got gud!");
+                console.log("You hit him for " + randomHitPoints + " hit points");
+                zombieHealth -= randomHitPoints;
+            } else if (userInput + 1 === randomToGuess || userInput - 1 === randomToGuess) {
+                console.log("You hit him for " + randomHitPoints + " hit points");
+                zombieHealth -=randomHitPoints;
+            } else {
+                console.log("You missed and he got you");
+                console.log("He hit you for " + randomHitPoints + " hit points");
+                userHealth -= randomHitPoints;
+            };
 
             if (zombieHealth <= 0) {
                 console.log("You struck the finishing blow. You're safe...for now.")
